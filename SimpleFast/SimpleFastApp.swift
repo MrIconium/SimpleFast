76ffcab50dbbd0107c6f4c41409f47b9159c7af9
MrIconium <mehmetemrahk@gmail.com>
2024-11-23 23:52:28 +0300

    Initial Commit

---
+++ SimpleFast/SimpleFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFastApp.swift
+//  SimpleFast
+//
+//  Created by Mehmet Emrah Konya on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleFastTests/SimpleFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFastTests.swift
+//  SimpleFastTests
+//
+//  Created by Mehmet Emrah Konya on 23.11.2024.
+//
+
+import Testing
+@testable import SimpleFast
+
+struct SimpleFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
